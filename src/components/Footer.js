973d1105a2bc9eb289fe9b0b029d4c10d2342a8e
nva_tng <anh.nguyen@thenewgym.vn>
2021-01-10 08:26:09 +0700

    init commit

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <a href="" target="_blank" rel="noreferrer noopener">
+          KTLT 2020
+        </a>
+        {" "}
+        <a href="" target="_blank" rel="noreferrer noopener">
+          Powered by Group 6
+        </a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
